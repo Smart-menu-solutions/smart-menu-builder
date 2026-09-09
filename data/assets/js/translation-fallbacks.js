ae73293db6273bf429700c7d0c4c6b4480d8dd9e
Smart-menu-solutions <smartmenusolutions@outlook.com>
2026-09-09 15:07:43 +0300

    Fix Italian and Spanish menu translations

--- data/assets/js/translation-fallbacks.js
+++ data/assets/js/translation-fallbacks.js
@@ -63,6 +63,58 @@ window.MENU_TRANSLATION_FALLBACKS = {
         Cappuccino: ['Καπουτσίνο', ''], Espresso: ['Εσπρέσο', ''], Tee: ['Τσάι', ''],
         'Weißwein 0.2l': ['Λευκό Κρασί 0,2l', ''], 'Rotwein 0.2l': ['Κόκκινο Κρασί 0,2l', '']
       }
+    },
+    it: {
+      categories: {
+        'Vorspeisen': 'Antipasti', 'Hauptgerichte': 'Piatti principali', 'Fischgerichte': 'Piatti di pesce',
+        'Vegetarische Gerichte': 'Piatti vegetariani', 'Desserts': 'Dessert', 'Getränke': 'Bevande'
+      },
+      items: {
+        'Bayerische Brezen': ['Pretzel bavarese', 'Pretzel morbido con formaggio Obazda'],
+        'Kartoffelsuppe': ['Zuppa di patate', 'Zuppa di patate fatta in casa con pancetta'],
+        'Gemischter Salat': ['Insalata mista', 'Insalata fresca con condimento della casa'],
+        'Wiener Schnitzel': ['Wiener Schnitzel', 'Cotoletta di vitello con patatine e insalata'],
+        'Jägerschnitzel': ['Cotoletta del cacciatore', 'Cotoletta di maiale con salsa cremosa ai funghi e spaetzle'],
+        'Schweinshaxe': ['Stinco di maiale', 'Stinco di maiale croccante con crauti e canederli di patate'],
+        'Sauerbraten': ['Sauerbraten', 'Arrosto di manzo renano con cavolo rosso e canederli'],
+        'Bratwurst Teller': ['Piatto di bratwurst', 'Tre salsicce di Norimberga con crauti e purè di patate'],
+        'Rinderroulade': ['Involtino di manzo', 'Ripieno di pancetta, cetriolini e cipolle, con cavolo rosso'],
+        'Gebratenes Lachsfilet': ['Filetto di salmone in padella', 'Con patate alle erbe e verdure'],
+        'Matjesfilet Hausfrauenart': ['Filetto di aringa Matjes', 'Con patate saltate'],
+        'Käsespätzle': ['Spätzle al formaggio', 'Con cipolle rosolate e insalata'],
+        'Gemüsepfanne': ['Padellata di verdure', 'Verdure di stagione con patate alle erbe'],
+        'Ofenkartoffel': ['Patata al forno', 'Con quark alle erbe e insalata'],
+        'Apfelstrudel': ['Strudel di mele', 'Con salsa alla vaniglia'],
+        'Schwarzwälder Kirschtorte': ['Torta Foresta Nera', 'Ricetta originale della casa'],
+        'Warmer Schokoladenkuchen': ['Torta al cioccolato calda', 'Con gelato alla vaniglia'],
+        'Kaffee': ['Caffè', ''], Cappuccino: ['Cappuccino', ''], Espresso: ['Espresso', ''], Tee: ['Tè', '']
+      }
+    },
+    es: {
+      categories: {
+        'Vorspeisen': 'Entrantes', 'Hauptgerichte': 'Platos principales', 'Fischgerichte': 'Platos de pescado',
+        'Vegetarische Gerichte': 'Platos vegetarianos', 'Desserts': 'Postres', 'Getränke': 'Bebidas'
+      },
+      items: {
+        'Bayerische Brezen': ['Pretzel bávaro', 'Pretzel blando con queso Obazda'],
+        'Kartoffelsuppe': ['Sopa de patata', 'Sopa casera de patata con tocino'],
+        'Gemischter Salat': ['Ensalada mixta', 'Ensalada fresca con aderezo de la casa'],
+        'Wiener Schnitzel': ['Escalope vienés', 'Escalope de ternera con patatas fritas y ensalada'],
+        'Jägerschnitzel': ['Escalope del cazador', 'Escalope de cerdo con salsa cremosa de champiñones y spaetzle'],
+        'Schweinshaxe': ['Codillo de cerdo', 'Codillo crujiente con chucrut y albóndigas de patata'],
+        'Sauerbraten': ['Sauerbraten', 'Asado renano con lombarda y albóndigas'],
+        'Bratwurst Teller': ['Plato de bratwurst', 'Tres salchichas de Núremberg con chucrut y puré de patata'],
+        'Rinderroulade': ['Rollo de ternera', 'Relleno de tocino, pepinillos y cebolla, con lombarda'],
+        'Gebratenes Lachsfilet': ['Filete de salmón a la sartén', 'Con patatas a las hierbas y verduras'],
+        'Matjesfilet Hausfrauenart': ['Filete de arenque Matjes', 'Con patatas salteadas'],
+        'Käsespätzle': ['Spaetzle con queso', 'Con cebolla dorada y ensalada'],
+        'Gemüsepfanne': ['Salteado de verduras', 'Verduras de temporada con patatas a las hierbas'],
+        'Ofenkartoffel': ['Patata al horno', 'Con quark a las hierbas y ensalada'],
+        'Apfelstrudel': ['Strudel de manzana', 'Con salsa de vainilla'],
+        'Schwarzwälder Kirschtorte': ['Tarta Selva Negra', 'Receta original de la casa'],
+        'Warmer Schokoladenkuchen': ['Pastel de chocolate caliente', 'Con helado de vainilla'],
+        'Kaffee': ['Café', ''], Cappuccino: ['Cappuccino', ''], Espresso: ['Espresso', ''], Tee: ['Té', '']
+      }
     }
   }
 };
